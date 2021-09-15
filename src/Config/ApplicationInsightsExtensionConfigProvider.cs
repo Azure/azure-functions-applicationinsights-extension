8f35cb3ffdbf9927aa19065b7cd10379b2cc7251
Graham Zuber <grzuber@microsoft.com>
2021-09-14 19:08:06 -0700

    Initial commit for Application Insights Extension.

---
+++ src/Config/ApplicationInsightsExtensionConfigProvider.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Azure.WebJobs.Description;
+using Microsoft.Azure.WebJobs.Host.Config;
+using Microsoft.Azure.WebJobs.Logging.ApplicationInsights;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace Microsoft.Azure.WebJobs.Extensions.ApplicationInsights
+{
+    [Extension("ApplicationInsights")]
+    internal class ApplicationInsightsExtensionConfigProvider : IExtensionConfigProvider
+    {
+        private readonly IConfiguration _configuration;
+        private readonly INameResolver _nameResolver;
+        private readonly ApplicationInsightsLoggerOptions _options;
+        private readonly ILoggerFactory _loggerFactory;
+
+        public ApplicationInsightsExtensionConfigProvider(
+            IOptions<ApplicationInsightsLoggerOptions> options,
+            IConfiguration configuration,
+            INameResolver nameResolver,
+            ILoggerFactory loggerFactory)
+        {
+            _configuration = configuration;
+            _nameResolver = nameResolver;
+            _options = options.Value;
+            _loggerFactory = loggerFactory;
+        }
+
+        /// <inheritdoc />
+        public void Initialize(ExtensionConfigContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            return;
+        }
+    }
+}
